466bf1f5f8ad911c7cd9cd104276a18306916bb6
Brandon Doyle <63417281+b-d-doyle@users.noreply.github.com>
2026-02-20 16:16:37 -0500

    formatting
    Removed the spacings that were confusing me. -B

--- GRITcode.ino
+++ GRITcode.ino
@@ -39,6 +39,13 @@
 //Brandon Doyle
 //Modification:
 //  - Edge detection: Added debouncing
+//
+//v1.1.3
+//20 February 2026
+//Brandon Doyle
+//Modification:
+//  - Spacing changed so I stop getting confused
+//  - Make contact LED blink when armed and unconnected
 
 const char softwareVersion[] = "v1.1.2";
 
@@ -115,64 +122,67 @@ void setup() {
 }
 
 void loop() {
-    // Read armed and contact logic
+  // Read armed and contact logic
   ARMED_LOGIC   = digitalRead(armed_pin_sig);
   CONTACT_LOGIC = digitalRead(contact_pin_sig);
 
-    // Show Logic by print and LED
+  // Show Logic by print and LED
   Serial.print("Armed Logic:");
   Serial.print(ARMED_LOGIC);
   Serial.print("\t Contact Logic:");
   Serial.println(CONTACT_LOGIC);
 
-    //Contact Light indication
+  //Contact Light indication
   if(CONTACT_LOGIC==true){
-       digitalWrite(contact_pin_LED, HIGH);    }
+    digitalWrite(contact_pin_LED, HIGH);
+  }
   if(CONTACT_LOGIC==false){
-       digitalWrite(contact_pin_LED,  LOW);    
-       delay(25);
-       digitalWrite(contact_pin_LED,  HIGH);
-       delay(25);}
+    digitalWrite(contact_pin_LED,  LOW);    
+    delay(25);
+    digitalWrite(contact_pin_LED,  HIGH);
+    delay(25);
+  }
   if(ARMED_LOGIC==false){
-       digitalWrite(armed_pin_LED,  LOW);    }
+    digitalWrite(armed_pin_LED,  LOW);
+  }
 
 
   while(ARMED_LOGIC==true){
     
-      // Read armed and contact logic
-       ARMED_LOGIC   = digitalRead(armed_pin_sig);
-       //CONTACT_LOGIC = digitalRead(contact_pin_sig);  //Now handled in Falling Edge detection -Brandon
+    // Read armed and contact logic
+    ARMED_LOGIC   = digitalRead(armed_pin_sig);
+    //CONTACT_LOGIC = digitalRead(contact_pin_sig);  //Now handled in Falling Edge detection -Brandon
 
-      //For debugging falling edge detection: 
+    //For debugging falling edge detection: 
+    // for(int i=8;i--;i>=0){
+    //   Serial.print(bitRead(CONTACT_LOGIC,i));
+    // }
+    // Serial.print("   ");
+    // Serial.println(CONTACT_LOGIC==0xF0);
+
+    digitalWrite(armed_pin_LED,  HIGH);
+    // Falling event: triggers on falling edge (hehe) of signal from contact_pin_sig
+    if( FE(digitalRead(contact_pin_sig),CONTACT_LOGIC) ){ // FE() stores current state and checks for falling edge
+      //Activating Launcher
+      delay(50);
+      digitalWrite(relay_pin_out,   HIGH);
+
+      //For debugging falling edge detection:
       // for(int i=8;i--;i>=0){
       //   Serial.print(bitRead(CONTACT_LOGIC,i));
       // }
       // Serial.print("   ");
       // Serial.println(CONTACT_LOGIC==0xF0);
+      
+      Serial.print("### Firing Solenoid ### \n");
+      //Pulse length
+      delay(250);
+      digitalWrite(relay_pin_out, LOW);
 
-       digitalWrite(armed_pin_LED,  HIGH);
-       // Falling event: triggers on falling edge (hehe) of signal from contact_pin_sig
-       if( FE(digitalRead(contact_pin_sig),CONTACT_LOGIC) ){ // FE() stores current state and checks for falling edge
-              //Activating Launcher
-            delay(50);
-            digitalWrite(relay_pin_out,   HIGH);
-
-            //For debugging falling edge detection:
-            // for(int i=8;i--;i>=0){
-            //   Serial.print(bitRead(CONTACT_LOGIC,i));
-            // }
-            // Serial.print("   ");
-            // Serial.println(CONTACT_LOGIC==0xF0);
-            
-            Serial.print("### Firing Solenoid ### \n");
-            //Pulse length
-            delay(250);
-            digitalWrite(relay_pin_out, LOW);
-
-            //delay(240000);   // 4 min cooldown (enough time to turn off the arm or power switch)
-            while(true){       // infinity min cooldown.
-              end_state_LED_pattern(); //Add specific end state patern
-            }
+      //delay(240000);   // 4 min cooldown (enough time to turn off the arm or power switch)
+      while(true){       // infinity min cooldown.
+        end_state_LED_pattern(); //Add specific end state patern
+      }
     }
   } 
   
